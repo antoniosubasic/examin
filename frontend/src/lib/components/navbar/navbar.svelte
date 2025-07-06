6fb6f6f9d939cae2cb4be7e3f427f1b8f2f613e9
Antonio Subašić <antonio.subasic.public@gmail.com>
2025-07-06 18:03:20 +0200

    refactor(frontend/navbar): simplify class handling for navbar items

--- frontend/src/lib/components/navbar/navbar.svelte
+++ frontend/src/lib/components/navbar/navbar.svelte
@@ -18,21 +18,6 @@
     $: ({ sessionId } = $authStore);
     $: currentPath = page.url.pathname;
 
-    function getLinkClasses(path: string): string {
-        const baseClasses = `
-            group inline-flex h-9 w-max items-center justify-center
-            rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors
-            hover:bg-accent hover:text-accent-foreground
-            focus:bg-accent focus:text-accent-foreground focus:outline-none
-            disabled:pointer-events-none disabled:opacity-50
-            data-[active]:bg-accent/50 data-[state=open]:bg-accent/50
-        `;
-
-        return `${baseClasses} ${
-            currentPath.startsWith(path) ? "" : "text-muted-foreground"
-        }`;
-    }
-
     async function handleLogout() {
         loading = true;
 
@@ -73,7 +58,18 @@
                             <NavigationMenuItem>
                                 <NavigationMenuLink
                                     href={link}
-                                    class={getLinkClasses(link)}
+                                    class={[
+                                        `group inline-flex h-9 w-max items-center justify-center
+                                        rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors
+                                        hover:bg-accent hover:text-accent-foreground
+                                        focus:bg-accent focus:text-accent-foreground focus:outline-none
+                                        disabled:pointer-events-none disabled:opacity-50
+                                        data-[active]:bg-accent/50 data-[state=open]:bg-accent/50`,
+                                        currentPath === link ||
+                                        currentPath.startsWith(`${link}/`)
+                                            ? ""
+                                            : "text-muted-foreground",
+                                    ]}
                                 >
                                     {`${link.substring(1).charAt(0).toUpperCase()}${link.substring(2)}`}
                                 </NavigationMenuLink>
